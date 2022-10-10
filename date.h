2777a0bb432b18c9b95d68525ce6eb6cead65e55
Valeredeterre <82400602+Valeredeterre@users.noreply.github.com>
2022-10-10 20:50:59 +0200

    Add files via upload

---
+++ date.h
@@ -0,0 +1,9 @@
+class Date
+{
+public:
+    Date(int year, int month, int day);
+private:
+    int _year;
+    int _month;
+    int _day;
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "heure.h"
+
+int main()
+{
+    Hour h1;
+    std::cout << h1.get_hour() << std::endl;
+    return EXIT_SUCCESS;
+}
