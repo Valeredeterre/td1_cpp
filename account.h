8016e18964e96bd7823177c3a2a26c8c83ad058b
Valere Dumont <dumontvalere.vrd@gmail.com>
2022-10-16 14:29:50 +0200

    add account transation implementation

---
+++ account.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "client.h"
+#include "transaction.h"
+#include <vector>
+
+class Account
+{
+    public:
+        Account(Client client, float balance, int accountNumber, Date openingDate);
+        std::string get_account();
+        std::string get_transactions_history();
+        void add_transaction(Transaction transaction);
+        void add_balance(float amount);
+        void send_money(float amount, Account& account);
+        void withdraw_money(float amount);
+        void freeze_balance(bool state);
+    private:
+        Client _client;
+        float _balance;
+        int _accountNumber;
+        Date _openingDate;
+        bool _state;
+        std::vector<Transaction> _transactionsHystory;
+};
